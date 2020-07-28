c2781041a5dc312c784439e659e4c9215c94560d
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-07-28 14:34:18 +0900

    Added logger.

--- server/base.py
+++ server/base.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 class BaseApi:
 
-    def __init__(self, cfg):
+    def __init__(self, cfg, logger):
         self.cfg = cfg
+        self.logger = logger
 
     def version(self):
+        self.logger.info("show version")
         return "1.0"
 
 
@@ -15,6 +17,7 @@ class BaseApp(Flask):
         Flask.__init__(self, import_name)
         self.api = api
         self.cfg = api.cfg
+        self.logger = api.logger
 
         @self.route('/{}/version'.format(self.cfg['web_context']))
         def version():
