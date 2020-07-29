b7b0bf16e9a18a7180ab87d47fb745594f34c994
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-07-29 15:14:08 +0900

    Added default admin page.

--- server/base.py
+++ server/base.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, send_from_directory, session
 from flask_session import Session
 from jinja2.exceptions import TemplateNotFound
+import pytz
+import threading
+
+
+tz = pytz.timezone('Asia/Tokyo')
+
+def utc_to_local(utc_timestamp):
+    return datetime.fromisoformat("{}+00:00".format(utc_timestamp)).astimezone(tz).isoformat() if utc_timestamp else ""
+
+def convert_chatroom_to_dict(chatroom):
+    return {
+        "id": chatroom.id,
+        "experimentId": chatroom.experiment_id,
+        "users": chatroom.users,
+        "created": chatroom.created,
+        "modified": chatroom.modified,
+        "initiator": chatroom.initiator,
+        "closed": chatroom.closed,
+        "events": len(chatroom.events),
+        "poll_requests": chatroom.poll_requests
+    }
+
 
 class BaseApi:
 
@@ -8,8 +30,23 @@ class BaseApi:
         self.cfg = cfg
         self.logger = logger
 
+        self.mutex = threading.Lock()
+
+        self.users = {}
+
+        # Dictionaries organized by chatroom ids.
+        self.chatrooms = {}
+        self.chatroom_locks = {}
+        self.released_chatrooms = {}
+
     def version(self):
         return "1.0"
+
+    def get_chatrooms(self):
+        return {
+            "chatrooms": list(self.chatrooms.values()),
+            "released_chatrooms": list(self.released_chatrooms.values())
+        }
 
 
 class BaseApp(Flask):
@@ -41,6 +78,10 @@ class BaseApp(Flask):
         def index():
             return self.index()
 
+        @self.route('/{}/admin'.format(self.cfg['web_context']))
+        def admin():
+            return self.admin()
+
     def get_static(self, path):
         return send_from_directory('static', path)
 
@@ -56,3 +97,23 @@ class BaseApp(Flask):
             return render_template('index.html')
         except TemplateNotFound:
             return render_template('default_index.html')
+
+    def admin(self):
+        data = self.api.get_chatrooms()
+        chatrooms = [convert_chatroom_to_dict(chatroom) for chatroom in data['chatrooms']]
+        released_chatrooms = [convert_chatroom_to_dict(released_chatrooms)
+                                for released_chatrooms in data['released_chatrooms']]
+        try:
+            return render_template(
+                'admin.html',
+                chatrooms=chatrooms,
+                released_chatrooms=released_chatrooms,
+                experiment_id=self.cfg['experiment_id'],
+                utc_to_local=utc_to_local)
+        except TemplateNotFound:
+            return render_template(
+                'default_admin.html',
+                chatrooms=chatrooms,
+                released_chatrooms=released_chatrooms,
+                experiment_id=self.cfg['experiment_id'],
+                utc_to_local=utc_to_local)
