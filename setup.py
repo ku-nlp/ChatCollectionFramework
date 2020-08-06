09f11153c45fa3bfd0f65503956bfa9e471e7f3e
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-08-06 11:47:04 +0900

    Increased version from 0.1.0 to 0.2.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chat-collection-framework",
-    version="0.1.0",
+    version="0.2.0",
     author="Frederic Bergeron",
     author_email="bergeron@nlp.ist.i.kyoto-u.ac.jp",
     description="A simple framework to implement a basic chat web application.",
