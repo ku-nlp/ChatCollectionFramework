8899dcf0b114e894c42bb5d05ef5f05846966079
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-09-09 14:14:36 +0900

    Start and stop the server.

--- tests/chat_test.py
+++ tests/chat_test.py
@@ -1,9 +1,11 @@
 import os
 import os.path
+import psutil
 import pytest
+import signal
 import subprocess
+import time
 
-@pytest.fixture
 def run_command(command):
     stream = os.popen(command)
     output = stream.read()
@@ -19,5 +21,24 @@ def test_start_server():
     print(f"ls -l={run_command('ls -l')}")
     print(f"which python={run_command('which python')}")
     print(f"python --version={run_command('python --version')}")
+
+    server_process = subprocess.Popen(["python App.py"], shell=True)
+
+    try:
+        print(f"server_process={server_process.pid}")
+
+        # Wait 10 seconds to make sure that the server has started properly.
+        time.sleep(10)
+
+    finally:
+        # Kill the server and its children processes.
+        parent = psutil.Process(server_process.pid)
+        children = parent.children(recursive=True)
+        for process in children:
+            process.send_signal(signal.SIGTERM)
+        server_process.terminate()
+
+
+
 
     assert 1 == 1
