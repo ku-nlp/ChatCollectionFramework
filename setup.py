b152bd0213f3708a4d87747eec496e6ddb88ecd3
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-09-16 17:58:51 +0900

    Added dependencies.

--- setup.py
+++ setup.py
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/ku_nlp/chatcollectionframework",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'flask-session',
+        'pytz',
+        'python-dateutil'
+    ],
     data_files=[
         ('static', [
             'static/default_chat_prologue.js',
